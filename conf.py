2e8a0bfe21ac769dd27a0c0273eea9faea1449c0
wrist <stoicheia1986@gmail.com>
2018-01-08 22:49:37 +0900

    Changed theme from libretto to bootstrap4, and modified posts/1.md

--- conf.py
+++ conf.py
@@ -142,8 +142,8 @@ NAVIGATION_LINKS = {
 }
 
 # Name of the theme to use.
-# THEME = "bootstrap3"
-THEME = "libretto"
+THEME = "bootstrap4"
+# THEME = "libretto"
 
 # Primary color of your theme. This will be used to customize your theme and
 # auto-generate related colors in POSTS_SECTION_COLORS. Must be a HEX value.
